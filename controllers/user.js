588e255f0b538d3b0d5c80b8437307ceedfcd427
kong thinkpad <skong03@gmail.com>
2022-07-10 20:23:57 -0700

    change the table schema

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,13 @@
 const db = require('../services/db');
+const { v4: uuidv4 } = require('uuid');
 
 const newUser = (req, res, next) =>{
     console.log(req.body.name);
+    //uuid is passed in userid. as reference of outside userId.
+    const uuid=uuidv4();
+    
     db.query("INSERT INTO refer_user(companyId, name, status, referenceId, email) VALUES ($1, $2, $3, $4, $5);"
-    ,[1, req.body.name, 'new', '123e4567-e89b-12d3-a456-426655440000', req.body.email]);
+    ,[1, req.body.name, 'new', uuid, req.body.email]);
     res.json({message: "POST new user"});
 };
 
--- sql/init.sql
+++ sql/init.sql
@@ -1,18 +1,25 @@
 CREATE TYPE user_status AS ENUM ('new', 'accept', 'paid');
-CREATE TABLE refer_user (
-    ID  SERIAL PRIMARY KEY,
-    companyId BIGINT,
-    name text,
-    status user_status,
-    referenceId VARCHAR(36) NOT NULL,
-    email VARCHAR(256) NOT NULL,
-    balance decimal(10,2)
-);
+/*
+id: auto increase Id
+companyId: org/companyId
+name: name
+status: userStatus
+externalId: id mapping
+email: email
+balance: current balance
+referalId: referalId
+contribution: contribution from current user to referalId
+*/
+CREATE TABLE refer_user (ID  SERIAL PRIMARY KEY, companyId BIGINT, name text, status user_status, externalId VARCHAR(36), email VARCHAR(256) NOT NULL, balance decimal(10,2), referalId BIGINT, contribution decimal(10,2));
 
-psql 'copy from heroku'
 
-CREATE TABLE refer_user (ID  SERIAL PRIMARY KEY, companyId BIGINT, name text, status user_status, referenceId VARCHAR(36) NOT NULL, email VARCHAR(256) NOT NULL, balance decimal(10,2));
+psql 'postgres://lfkvqpotyzxbzh:73b94a1d0e9068c35989ed8fe0873f394cf75ca3796d87a30c62063d1ce06de3@ec2-44-206-11-200.compute-1.amazonaws.com:5432/df6be3uva5dtsc'
+--show table
+\dt
+DROP TABLE refer_user
 
-INSERT INTO refer_user(companyId, name, status, referenceId, email) VALUES ('1', 'bob', 'new', '123e4567-e89b-12d3-a456-426655440000', 'a@b.com');
+
+
+INSERT INTO refer_user(companyId, name, status, externalId, email, balance) VALUES ('1', 'bob', 'new', '123e4567-e89b-12d3-a456-426655440000', 'a@b.com', 0);
 
 select * from refer_user;
