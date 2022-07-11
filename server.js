daacea482fef4a3b88ba7cf39c8c4dd5473e6fde
kong thinkpad <skong03@gmail.com>
2022-07-10 21:57:47 -0700

    setup client

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const userRoutes = require('./routes/user'); // import the routes
 
 const app = express();
@@ -7,13 +8,20 @@ const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port ' + listener.address().port)
 })
 
+app.use(express.static(path.resolve(__dirname, './my-app/build')));
+
 // Example
-app.get("/", function (req, res) {
-  res.json({message: "Hello world!"});
-});
+// app.get("/", function (req, res) {
+//   res.json({message: "Hello world!"});
+// });
 
 app.use("/", userRoutes);
 
 app.get("/close", function(){
     process.exit();
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './my-app/build', 'index.html'));
 });
